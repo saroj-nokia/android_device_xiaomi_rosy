fa4659843194874c1cd20b74bb7d905c11f046fe
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2024-07-26 15:40:27 +0545

    rosy: Enforce vintf kernel requirements

--- device.mk
+++ device.mk
@@ -27,6 +27,8 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
  
 # APEX
 PRODUCT_COMPRESSED_APEX := false
