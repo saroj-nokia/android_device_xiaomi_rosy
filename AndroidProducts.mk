e7f5b43dcfd0ba121fda3b0e27c39df6c0b7ede0
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2024-07-30 10:07:01 +0545

    rosy:pixelos bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rosy.mk
+    $(LOCAL_DIR)/aosp_rosy.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rosy-user \
-    lineage_rosy-userdebug \
-    lineage_rosy-eng
+    aosp_rosy-user \
+    aosp_rosy-userdebug \
+    aosp_rosy-eng
