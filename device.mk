3818cbaedeed4d6be2ba2d441c121698b3275e77
Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2024-08-29 19:44:40 +0545

    rosy: switch PRODUCT_SYSTEM_SERVER_COMPILER_FILTER to balanced-profile
    Signed-off-by: Saroj Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -20,8 +20,8 @@ $(call inherit-product, vendor/xiaomi/rosy/rosy-vendor.mk)
 TARGET_BOARD_PLATFORM := msm8953
 TARGET_BOARD_SUFFIX := _64
 
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+# balanced profile services and wifi-service
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := balanced-profile
 
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
  
