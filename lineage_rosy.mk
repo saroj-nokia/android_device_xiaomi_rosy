fdb7af0b8086e8d8b00f71f1735dcf092bd5e425
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-12-14 09:46:02 +0545

    Update lineage_rosy.mk
    Signed-off-by: Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>

--- lineage_rosy.mk
+++ lineage_rosy.mk
@@ -49,3 +49,12 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := rosy
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="rosy-user 8.1.0 OPM1.171019.026 V11.0.2.0.ODAMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/rosy/rosy:8.1.0/OPM1.171019.026/V11.0.2.0.ODAMIXM:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
