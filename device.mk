bc5c49ac2d055107eac4d51ab411acfb5da5f118
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2026-01-07 07:34:18 +0000

    Revert "rosy: add SysUI to PRODUCT_DEXPREOPT_SPEED_APPS"
    This reverts commit 4bbaadfdfa0ab31b6a47a03ec7cb983a3135232a.

--- device.mk
+++ device.mk
@@ -153,10 +153,6 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
-# Dexpreopt - SystemUI
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUIGoogle
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
